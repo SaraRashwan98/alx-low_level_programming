95c4128a45bd4d17823905a580d40e89b19dd03b
SaraRashwan98 <sararashwan98@yahoo.com>
2023-06-29 18:25:04 -0700

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,7 +10,7 @@ char *string_toupper(char *)
 {
 	int i = 0;
 	
-	while (s[i] >= '\0')
+	while (s[i] != '\0')
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
 		{
@@ -18,5 +18,6 @@ char *string_toupper(char *)
 		}
 		i++;
 	}
+	
 	return (s);
 }
